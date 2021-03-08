4b140836d8b312e9458b0957e7a6835d000b39cc
Гура Илья <IGura@softrust.ru>
2021-03-08 21:20:11 +0300

    2.

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . './block.php';
-include $_SERVER['DOCUMENT_ROOT'] . './include/success.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/block.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/include/success.php';
 
 ?>
 <!DOCTYPE html>
